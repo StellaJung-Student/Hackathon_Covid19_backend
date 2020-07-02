526a43572f72b40cc22b44441524c05c9c095a33
Stella Jung <sojung@myseneca.ca>
2020-07-02 11:22:20 -0400

    added url in company model

--- src/db/company.js
+++ src/db/company.js
@@ -12,6 +12,11 @@ const companySchema = mongoose.Schema(
       required: [true, 'Address is required'],
     },
     description: String,
+    url: {
+      type: String,
+      default:
+        'https://www.pngfind.com/pngs/m/665-6659827_enterprise-comments-default-company-logo-png-transparent-png.png',
+    },
   },
   {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
